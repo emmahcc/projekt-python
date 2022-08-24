69165c185a1a0720ca302ed56b298cda730dabf9
emmahcc <111755583+emmahcc@users.noreply.github.com>
2022-08-24 19:52:52 +0200

    Uppdatering

--- numberguessinggame.py
+++ numberguessinggame.py
@@ -4,17 +4,21 @@ import random
 print('Jag(programmet) tänker på ett nummer mellan 1 till 10, gissa vilket det är!')
 num = random.randint(1, 10)
 user_num= None
+number_guesses = 1
 
 while user_num != num:
-    user_num = int(input('Gissa på ett nummer:'))
-    
+    try:
+        user_num = int(input('Gissa på ett nummer:'))
+    except (ValueError):
+        print("Det var inte ett nummer!")
+        user_num = int(input('Gissa på ett nummer:'))
     if user_num == num:
-        print('RÄTT! Bra gissat')
+        print('RÄTT! Bra gissat. Du klarade det på '+ str(number_guesses) + ' försök.')
+        number_guesses = number_guesses +1
         break
-    
     elif user_num < num:
         print(str(user_num) + ' är för lågt, gissa igen')
-    
+        number_guesses = number_guesses +1
     elif user_num > num:
         print(str(user_num) + ' är för högt, gissa igen')
-  
+    number_guesses = number_guesses +1
